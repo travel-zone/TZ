6ca0fa38338f55a23d3ae2ba8ab1e31384ed6a20
Gabriela E. Figueroa Riquelme <elizabehtriquelme@gmail.com>
2019-07-02 23:22:49 -0300

    cambios
    Driver modificad, se debe de eliminar la clase randomcreate.

--- Tuber/src/Tuber/Driver.java
+++ Tuber/src/Tuber/Driver.java
@@ -1,15 +1,43 @@
 package Tuber;
 
+import java.util.Random;
+
 public class Driver {
-	private int edad_conductor;
-	private byte experiencia_conductor; //	(*)
-	//	matricula?
+	//	Atributos 
+	Random rand = new Random();
+	public String driver_name;
+	public String driver_age;
+	public byte driver_personality;
 	
-	public void driverName() {
-		Randomcreate r = new Randomcreate();
-		r.randomName();
-		}
+	//	Data para el conductor
+	String[] nombres = new String[] {"Elías", "Juan", "Carmen", "Jennifer","Nicolas","Gabriela","Manuel","Macarena","Daniel","Elizabeth","Felipe","Matias","Catalina"};
+	String[] apellidos = new String[] {"Muñoz", "Fernández", "Soto", "Troncoso","Pérez","Gonzales","Silva","Espinoza","Molina","Sandoval","Escobar","Alarcon","Correa"};
+	String[] num1 = new String[] {"2","3","4","5",};
+	String[] num2 = new String[] {"0","1","2","3","4","5","6","7","8","9"};
+	
+	//	Creando conductor Random
+	public Driver() {
+		String nombre1, apellido1, edad1, edad2;
+		Randomcreate obj1 = new Randomcreate();
+		
+		nombre1 = obj1.nombres[rand.nextInt(obj1.nombres.length)];
+		apellido1 = obj1.apellidos[rand.nextInt(obj1.apellidos.length)];
+		edad1 = obj1.num1[rand.nextInt(obj1.num1.length)];
+		edad2 = obj1.num2[rand.nextInt(obj1.num2.length)];
+
+		driver_name = nombre1 + " " + apellido1;
+		driver_age = edad1 + edad2;
+
 	}
 	
+	public String getDrivername() {
+		return driver_name;
+	}
+	
+	public String getDriverage() {
+		return driver_age;
+	}
+}
+
 	
 
