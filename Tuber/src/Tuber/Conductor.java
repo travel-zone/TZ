2a06c29548459e85de5cc0d1598069fe63d6e8be
Gabriela E. Figueroa Riquelme <elizabehtriquelme@gmail.com>
2019-07-03 03:09:32 -0300

    Cambios
    Se incorporó las librerías del profesor, se decide no utilizarlas por problemas de compatibilidad y por demora del proyecto.

--- Tuber/src/Tuber/Conductor.java
+++ Tuber/src/Tuber/Conductor.java
@@ -1,10 +1,12 @@
 package Tuber;
-
+import tuber.drivers.DriverBehavior;
 import java.util.Random;
+import tuber.types.Driver;
 
-public class Driver {
-	//	Atributos 
+public class Conductor extends DriverBehavior implements Driver {
 	Random rand = new Random();
+	
+	//	Atributos 
 	public String driver_name;
 	public String driver_age;
 	public byte driver_personality;
@@ -16,18 +18,16 @@ public class Driver {
 	String[] num2 = new String[] {"0","1","2","3","4","5","6","7","8","9"};
 	
 	//	Creando conductor Random
-	public Driver() {
+	public Conductor() {
 		String nombre1, apellido1, edad1, edad2;
-		Randomcreate obj1 = new Randomcreate();
 		
-		nombre1 = obj1.nombres[rand.nextInt(obj1.nombres.length)];
-		apellido1 = obj1.apellidos[rand.nextInt(obj1.apellidos.length)];
-		edad1 = obj1.num1[rand.nextInt(obj1.num1.length)];
-		edad2 = obj1.num2[rand.nextInt(obj1.num2.length)];
+		nombre1 = nombres[rand.nextInt(nombres.length)];
+		apellido1 = apellidos[rand.nextInt(apellidos.length)];
+		edad1 = num1[rand.nextInt(num1.length)];
+		edad2 = num2[rand.nextInt(num2.length)];
 
 		driver_name = nombre1 + " " + apellido1;
 		driver_age = edad1 + edad2;
-
 	}
 	
 	public String getDrivername() {
@@ -37,6 +37,32 @@ public class Driver {
 	public String getDriverage() {
 		return driver_age;
 	}
+	public void dataDriver() {
+		Conductor obj = new Conductor();
+		System.out.println("Nombre del conductor/a: "+obj.driver_name);
+		System.out.println("Edad: "+(obj.driver_age)+ " años." );
+	}
+	
+
+	@Override
+	public boolean busy() {
+		return false;
+	}
+
+	@Override
+	public int getPersonality() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean likeLargeServices() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+		
+	
+	 
 }
 
 	
