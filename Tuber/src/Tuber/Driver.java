38142709712c5be7b84aba94b373ce1a814148a6
Gabriela E. Figueroa Riquelme <elizabehtriquelme@gmail.com>
2019-07-04 06:52:53 -0300

    Cambios
    Se le agregó al Driver su personalidad

--- Tuber/src/Tuber/Driver.java
+++ Tuber/src/Tuber/Driver.java
@@ -1,33 +1,39 @@
 package Tuber;
-import tuber.drivers.DriverBehavior;
 import java.util.Random;
-import tuber.types.Driver;
 
-public class Conductor extends DriverBehavior implements Driver {
+public class Driver {
 	Random rand = new Random();
 	
 	//	Atributos 
 	public String driver_name;
 	public String driver_age;
-	public byte driver_personality;
+	public int driver_personality;
+	public int cantidad;
+	public String posicion;
 	
 	//	Data para el conductor
 	String[] nombres = new String[] {"Elías", "Juan", "Carmen", "Jennifer","Nicolas","Gabriela","Manuel","Macarena","Daniel","Elizabeth","Felipe","Matias","Catalina"};
 	String[] apellidos = new String[] {"Muñoz", "Fernández", "Soto", "Troncoso","Pérez","Gonzales","Silva","Espinoza","Molina","Sandoval","Escobar","Alarcon","Correa"};
 	String[] num1 = new String[] {"2","3","4","5",};
 	String[] num2 = new String[] {"0","1","2","3","4","5","6","7","8","9"};
+	int[] rango_personalidad = new int[] {1,2,3,4,5};
+	
 	
 	//	Creando conductor Random
-	public Conductor() {
+	public Driver() {
 		String nombre1, apellido1, edad1, edad2;
+		int personalidad1;
 		
 		nombre1 = nombres[rand.nextInt(nombres.length)];
 		apellido1 = apellidos[rand.nextInt(apellidos.length)];
 		edad1 = num1[rand.nextInt(num1.length)];
 		edad2 = num2[rand.nextInt(num2.length)];
-
+		personalidad1 = rango_personalidad[rand.nextInt(rango_personalidad.length)];
+		
 		driver_name = nombre1 + " " + apellido1;
 		driver_age = edad1 + edad2;
+		driver_personality = personalidad1;
+		
 	}
 	
 	public String getDrivername() {
@@ -38,30 +44,13 @@ public class Conductor extends DriverBehavior implements Driver {
 		return driver_age;
 	}
 	public void dataDriver() {
-		Conductor obj = new Conductor();
-		System.out.println("Nombre del conductor/a: "+obj.driver_name);
+		Driver obj = new Driver();
+		System.out.println("Nombres del conductor/a: "+obj.driver_name);
 		System.out.println("Edad: "+(obj.driver_age)+ " años." );
 	}
-	
-
-	@Override
-	public boolean busy() {
-		return false;
-	}
-
-	@Override
 	public int getPersonality() {
-		// TODO Auto-generated method stub
-		return 0;
+		return driver_personality;
 	}
-
-	@Override
-	public boolean likeLargeServices() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-		
-	
 	 
 }
 
