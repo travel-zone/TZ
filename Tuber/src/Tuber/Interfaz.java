ff104737bd1086edb0c10e649e544c402a851659
Nicolas Hidalgo <nickolashidalgo@gmail.com>
2019-07-04 20:35:37 -0400

    Actualizacion de clases"Interfaz" y "mapa"
    Actualizacion agregando funciones y solucionando problemas

--- Tuber/src/Tuber/Interfaz.java
+++ Tuber/src/Tuber/Interfaz.java
@@ -13,17 +13,27 @@ public class Interfaz {
 
 		//	Entregando coordenada al Pasajero
 		if(pasajero.getQuerying() == true) {
-			pasajero.posicion = obj.getStart();
-			System.out.println("El cliente se encuentra en: "+pasajero.posicion);
-			conductor.cantidad = rand.nextInt(maximo_conductores);	//	Conductores dentro del radio del pasajero
+			conductor.cantidad = rand.nextInt(maximo_conductores);	//	Conductores dentro del radio del pasajero;
+			String[] cantidad_conductores = new String[obj.getPosicion().length];
+			double[] cantidad_latitud = new double[obj.getPosicion().length];
+			double[] cantidad_longitud = new double[obj.getPosicion().length];
+			conductor.posicion = obj.getPosicion();
+			conductor.latitud = obj.getLat();
+			conductor.longitud = obj.getLon();
 			System.out.println("Conductores cercanos al pasajero: "+conductor.cantidad);
-			conductor.posicion = obj.getStart();
-
-			
-			
-		}else {
-			System.out.print("No se ha solicitado Tuber.");
+			for (int j=0; j<conductor.cantidad; j++) {
+					int i;
+					i = rand.nextInt(obj.getPosicion().length);	
+					cantidad_conductores[i] = conductor.posicion[i];
+					cantidad_latitud[i] = conductor.latitud[i];
+					cantidad_longitud[i] = conductor.longitud[i];
+					System.out.println("El conductor" +(j+1) + " se encuentra en: " +cantidad_conductores[i]);
+					System.out.println(cantidad_latitud[i]);
+					System.out.println(cantidad_longitud[i]);
+			}
+			}
 		}
-		
 	}
-}
+
+		
+		
--- Tuber/src/Tuber/Mapa.java
+++ Tuber/src/Tuber/Mapa.java
@@ -3,182 +3,46 @@ import java.util.Random;
 
 //TODO MOVIMIENTO SERA DE 0.01
 
-//latitud_superior = 33.33;
-//latitud_inferior = 33.64;
-//longitud_este = 70.49;
-//longitud_oeste = 70.82;
+//latitud_superior = -33.33;
+//latitud_inferior = -33.64;
+//longitud_este = -70.49;
+//longitud_oeste = -70.82;
 
 public class Mapa {
 	Random rand = new Random();
 	
-	public double diferencia_persona_latitud;
-	public double diferencia_persona_longitud;
-	public double distancia_recorrida;
-	public String lugar_partida;
-	public String lugar_llegada;
-	public int precio_viaje;
-	public String partida_conductor;
-	public double posicion_conductor_latitud;
-	public double posicion_conductor_longitud;
-	public double posicion_cliente_latitud;
-	public double posicion_cliente_longitud;
+	public String[] partida;
+	public double[] posicion_latitud;
+	public double[] posicion_longitud;
 	
 	//	CREACION POSICIONES RANDOM (
 	
-	String[] posicion = new String[] {"Pio Nono","Pdte Balmaceda","Av. Grecia","Veintitres de Febrero","Gran Torre Santiago"};
-	
+	String[] lugar = new String[] {"Pio Nono","Pdte Balmaceda","Av. Grecia","Veintitres de Febrero","Gran Torre Santiago","San Ramon","El Corcolen","Tucapel","Reyes Magos","Av. Quilin","Barrio el Golf","Azteca","Jose Arrieta","Algarrobo","Riquelme","Pedro Lobos","Av Independencia","Sebastian Elcano"};
+	double[] latitud  = new double[] { -33.43, -33.43, -33.46, -33.43, -33.42, -33.55, -33.58, -33.57, -33.52, -33.49, -33.42, -33.48, -33.46, -33.53, -33.54, -33.46, -33.40, -33.43};
+	double[] longitud = new double[] { -70.64, -70.67, -70.61, -70.55, -70.61, -70.64, -70.59, -70.66, -70.60, -70.61, -70.58, -70.57, -70.56, -70.62, -70.66, -70.57, -70.67, -70.57};
 	
 	public Mapa() {
-		String lugar;
-		String lugar1;
-		String lugar2;
-		lugar = posicion[rand.nextInt(posicion.length)];
-		lugar1 = posicion[rand.nextInt(posicion.length)];
-		lugar2 = posicion[rand.nextInt(posicion.length)];
-		//ESTE IF ES PARA BAJAR PROBABILIDADES DE QUE LA PARTIDA SEA IGUAL A LA LLEGADA
-		if (lugar == lugar1) {				
-			lugar1 = posicion[rand.nextInt(posicion.length)];
-		}
-		
-		double latitud = 0;
-		double latitud1 = 0;
-		double latitud2 = 0;
-		double longitud = 0;
-		double longitud1 = 0;
-		double longitud2 = 0;
-		
-	/*
-	   if (lugar == posicion[]) {	//
-		latitud = 
-		longitud = 
-		}
-	if (lugar1 == posicion[]) {
-		latitud1 = 
-		longitud1 = 
-		}
-	if (lugar2 == posicion[]) {
-		latitud2 = 
-		longitud2 = 
-		}
-	*/
-	
-	if (lugar == posicion[0]) {	//Pio Nono
-		latitud = -33.43;
-		longitud = -70.64;
-		}
-	if (lugar1 == posicion[0]) {	
-		latitud1 = -33.43;
-		longitud1 = -70.64;
-		}
-	if (lugar2 == posicion[0]) {
-		latitud2 = -33.43;
-		longitud2 = -70.64;
-		}
-	if (lugar == posicion[1]) {	//Pdte Balmaceda
-		latitud = -33.43;
-		longitud = -70.67;
-		}
-	if (lugar1 == posicion[1]) {	
-		latitud1 = -33.43;
-		longitud1 = -70.67;
-		}
-	if (lugar2 == posicion[1]) {
-		latitud2 = -33.43;
-		longitud2 = -70.67;
-		}
-	if (lugar == posicion[2]) {	//Av Grecia
-		latitud = -33.46;
-		longitud = -70.61;
-		}
-	if (lugar1 == posicion[2]) {	
-		latitud1 = -33.46;
-		longitud1 = -70.61;
-		}
-	if (lugar2 == posicion[2]) {
-		latitud2 = -33.46;
-		longitud2 = -70.61;
-		}
-	if (lugar == posicion[3]) {	//Veintitres de Febrero
-		latitud = -33.43;
-		longitud = -70.55;
-		}
-	if (lugar1 == posicion[3]) {
-		latitud1 = -33.43;
-		longitud1 = -70.55;
-		}
-	if (lugar2 == posicion[3]) {
-		latitud2 = -33.43;
-		longitud2 = -70.55;
-		}
-	if (lugar == posicion[4]) {	//Gran Torre Santiago
-		latitud = -33.42 ;
-		longitud = -70.61;
-		}
-	if (lugar1 == posicion[4]) {
-		latitud1 = -33.42;
-		longitud1 = -70.61;
-		}
-	if (lugar2 == posicion[4]) {
-		latitud2 = -33.42;
-		longitud2 = -70.61;
-		}
-	
-	diferencia_persona_latitud = -latitud + latitud1;
-	diferencia_persona_longitud = -longitud + longitud1;
-	lugar_partida = lugar;
-	lugar_llegada = lugar1;
-	partida_conductor = lugar2;
-	posicion_conductor_latitud = latitud2;
-	posicion_conductor_longitud = longitud2;
-	
-	
-	
-	if (diferencia_persona_latitud<0) {
-		diferencia_persona_latitud = -diferencia_persona_latitud;
+
+	partida = lugar;
+	posicion_latitud = latitud;
+	posicion_longitud = longitud;
 	}
-	if (diferencia_persona_longitud<0) {
-		diferencia_persona_longitud = -diferencia_persona_longitud;
+
+	public String[] getPosicion() {			//RETORNA EN QUE LUGAR SE ENCUENTRA EL CONDUCTOR
+		return partida;
 	}
 	
-	distancia_recorrida = (diferencia_persona_latitud + diferencia_persona_longitud)*100;
-	
-	precio_viaje = (int) (distancia_recorrida*1100);
+	public double[] getLat() {
+		return posicion_latitud;
 	}
 	
-	public String getStartDriver() {			//RETORNA EN QUE LUGAR SE ENCUENTRA EL CONDUCTOR
-		return partida_conductor;
-	}
-	public double getLatDriver() {				//RETORNA EN QUE LATITUD SE ENCUENTRA EL CONDUCTOR
-		return posicion_conductor_latitud;
-	}
-	public double getLonDriver() {				//RETORNA EN QUE LONGITUD SE ENCUENTRA EL CONDUCTOR
-		return posicion_conductor_longitud;
-	}
-	public String getStart() {					//RETORNA EN QUE LUGAR SE ENCUENTRA EL CLIENTE
-		return lugar_partida;
-	}
-	public double getLatClient() {				//RETORNA EN QUE LATITUD SE ENCUENTRA EL CLIENTE
-		return posicion_conductor_latitud;
-	}
-	public double getLonClient() {				//RETORNA EN QUE LONGITUD SE ENCUENTRA EL CLIENTE
-		return posicion_conductor_longitud;
-	}
-	public String getFinish(){					//RETORNA A QUE LUGAR QUIERE LLEGAR EL CLIENTE
-		return lugar_llegada;
-	}
-	public double getDistancia() {				//RETORNA CUANTA DISTANCIA ES RECORRIDA EN EL VIAJE SOLICITADO POR EL CLIENTE
-		return distancia_recorrida;
-	}
-	public double getPrice() {					//RETORNA EL VALOR TOTAL DEL VIAJE
-		return precio_viaje;
+	public double[] getLon() {
+		return posicion_longitud;
 	}
 	
 	public void dataMapa() {
 		Mapa obj = new Mapa();
-		System.out.println("Inicio de viaje: " + obj.lugar_partida);
-		System.out.println("Final de viaje: " + obj.lugar_llegada);
-		System.out.println("Distancia de viaje: " + Math.ceil(obj.distancia_recorrida));
-		System.out.println("Valor del viaje: " +obj.precio_viaje);
+		
 	}
 	
 }
